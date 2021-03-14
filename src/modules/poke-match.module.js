05588a5cf0cd79819cf4782044653802fbe7219f
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2021-03-14 17:35:16 +0000

    Add Start and Play screens

--- src/modules/poke-match.module.js
+++ src/modules/poke-match.module.js
@@ -1,5 +1,5 @@
 (function () {
   'use strict';
 
-  angular.module('PokeMatchApp', []);
+  angular.module('PokeMatchApp', ['ui.router']);
 }());
---
+++ src/routes.js
@@ -0,0 +1,25 @@
+(function () {
+
+  'use strict'
+
+  angular
+    .module('PokeMatchApp')
+    .config(RoutesConfig);
+
+  RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function RoutesConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('start', {
+        url: '/',
+        templateUrl: 'src/templates/start.html'
+      })
+      .state('play', {
+        url: '/',
+        templateUrl: 'src/templates/game.html',
+        controller: 'CardController as cardCtrl'
+      })
+  }
+
+})();
